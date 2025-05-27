652ed15235ffdf0a71d942cdfaaff13857c1cbc0
Your Name <you@example.com>
2025-05-27 20:29:40 +0530

    add config server with git

--- loans/src/main/java/com/travelbank/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/travelbank/loans/dto/LoansContactInfoDto.java
@@ -5,6 +5,15 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class LoansContactInfoDto {
+		
+	String message;
+	Map<String, String> contactDetails;
+	List<String> onCallSupport;
 }
